a629ce4cc6417c250eea703446b9d28baf45db33
Lina W. Sasmita <Linawsasmita@gmail.com>
2019-02-16 14:39:53 +0700

    fixing styling

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ const githubFellow = async () => {
          document.getElementById('github-follower').innerHTML =
             document.getElementById("github-follower").innerHTML +
             `<div>
-                <div class="card mt-3" style="width: 10 rem">
-                    <img class="card-img-top" src="${res.avatar_url}" alt="Follower Avatar />"
+                <div class="card mt-3" style="width: 15 rem">
+                    <img class="card-img-top" src="${res.avatar_url}" alt="Follower Avatar">
                     <div class="card-body">
-                    <h5 class="card_title">${res.login}</h5>
-                    <a href="${res.html_url}" class="btn btn-primary">Go to profile</a>
+                        <h5 class="card-title">${res.login}</h5>
+                        <a href="${res.html_url}" class="btn btn-primary">Go to profile</a>
                     </div>
                 </div>
             </div>`
